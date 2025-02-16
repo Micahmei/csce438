479ae93a4bbda30fac543010fa9b49f424573726
Micahmei <145519069+Micahmei@users.noreply.github.com>
2025-02-15 22:28:41 -0600

    Update tsd.cc

--- mp1_skeleton/tsd.cc
+++ mp1_skeleton/tsd.cc
@@ -1,463 +1,227 @@
-/*
- *
- * Copyright 2015, Google Inc.
- * All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are
- * met:
- *
- *     * Redistributions of source code must retain the above copyright
- * notice, this list of conditions and the following disclaimer.
- *     * Redistributions in binary form must reproduce the above
- * copyright notice, this list of conditions and the following disclaimer
- * in the documentation and/or other materials provided with the
- * distribution.
- *     * Neither the name of Google Inc. nor the names of its
- * contributors may be used to endorse or promote products derived from
- * this software without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
- * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
- * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
- * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
- * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
- * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
- * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
- * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
- * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
- * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
- * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- *
- */
-
-#include <ctime>
-
-#include <google/protobuf/timestamp.pb.h>
-#include <google/protobuf/duration.pb.h>
-
+#include <iostream>
+#include <memory>
+#include <thread>
+#include <vector>
+#include <string>
+#include <unistd.h>
+#include <grpc++/grpc++.h>
+#include <sys/stat.h>
+#include <grpcpp/grpcpp.h>
 #include <fstream>
 #include <filesystem>
-#include <iostream>
-#include <algorithm>
-#include <memory>
-#include <string>
-#include <stdlib.h>
-#include <unistd.h>
 #include <google/protobuf/util/time_util.h>
-#include <grpc++/grpc++.h>
-#define GLOG_EXPORT
-#define GLOG_NO_EXPORT
 #include <glog/logging.h>
-#define log(severity, msg) \
-  LOG(severity) << msg;    \
-  google::FlushLogFiles(google::severity);
 
 #include "sns.grpc.pb.h"
+#include "coordinator.grpc.pb.h"
 
-using csce438::ListReply;
-using csce438::Message;
-using csce438::Reply;
-using csce438::Request;
 using csce438::SNSService;
-using google::protobuf::Duration;
-using google::protobuf::Timestamp;
+using csce438::CoordService;
+using csce438::ID;
+using csce438::ServerInfo;
+using csce438::Confirmation;
 using grpc::Server;
 using grpc::ServerBuilder;
 using grpc::ServerContext;
-using grpc::ServerReader;
-using grpc::ServerReaderWriter;
-using grpc::ServerWriter;
 using grpc::Status;
+using grpc::ClientContext;
+using grpc::ClientWriter;
 
-// return local time
-std::time_t iso8601ToTimeT(const std::string &iso8601)
-{
-  std::tm tm = {};
-  std::istringstream ss(iso8601);
-
-  ss >> std::get_time(&tm, "%Y-%m-%dT%H:%M:%SZ");
-
-  if (ss.fail())
-  {
-    std::cerr << "Error: Failed to parse timestamp: " << iso8601 << std::endl;
-    return 0;
-  }
-
-  return timegm(&tm);
-}
-
-struct Post
-{
-  std::string username;
-  std::string post;
-  std::time_t timestamp;
-};
-
-struct Client
-{
-  std::string username;
-  bool connected = true;
-  int following_file_size = 0;
-  std::vector<Client *> client_followers;
-  std::vector<Client *> client_following;
-  ServerReaderWriter<Message, Message> *stream = 0;
-  bool operator==(const Client &c1) const
-  {
-    return (username == c1.username);
-  }
-};
-
-// Vector that stores every client that has been created
-std::vector<Client *> client_db;
-
-class SNSServiceImpl final : public SNSService::Service
-{
-
-  Status List(ServerContext *context, const Request *request, ListReply *list_reply) override
-  {
-    /*********
-    YOUR CODE HERE
-    **********/
-    std::string username = request->username();
-    for (const auto &client : client_db)
-    {
-      list_reply->add_all_users(client->username);
-    }
-
-    Client *current_user = nullptr;
-    for (const auto &client : client_db)
-    {
-      if (client->username == username)
-      {
-        current_user = client;
-        break;
-      }
-    }
-    for (const auto &follower : current_user->client_followers)
-    {
-      list_reply->add_followers(follower->username);
-    }
-
-    return Status::OK;
-  }
-
-  std::unordered_map<std::string, std::unordered_map<std::string, std::time_t>> follow_time;
-
-  Status Follow(ServerContext *context, const Request *request, Reply *reply) override
-  {
-
-    /*********
-    YOUR CODE HERE
-    **********/
-    // u1 follow -> u2
-    std::string follower_name = request->username();   // u1
-    std::string followee_name = request->arguments(0); // u2
-
-    if (follower_name == followee_name)
-    {
-      reply->set_msg("cannot follow self");
-      return Status::OK;
-    }
-
-    Client *follower = nullptr;
-    Client *followee = nullptr;
-
-    for (auto &client : client_db)
-    {
-      if (client->username == follower_name)
-      {
-        follower = client;
-      }
-      if (client->username == followee_name)
-      {
-        followee = client;
-      }
-    }
-
-    if (followee == nullptr)
-    {
-      reply->set_msg("followed user no exist");
-      return Status::OK;
-    }
-
-    for (const auto &following : follower->client_following)
-    {
-      if (following->username == followee_name)
-      {
-        reply->set_msg("already following user");
-        return Status::OK;
-      }
-    }
-
-    follower->client_following.push_back(followee);
-    followee->client_followers.push_back(follower);
-
-    follow_time[follower_name][followee_name] = std::time(nullptr);
-
-    reply->set_msg("followed successful");
-
-    return Status::OK;
-  }
-
-  Status UnFollow(ServerContext *context, const Request *request, Reply *reply) override
-  {
-
-    /*********
-    YOUR CODE HERE
-    **********/
-    std::string username = request->username();
-    std::string unfollow_username = request->arguments(0);
-
-    if (username == unfollow_username)
-    {
-      reply->set_msg("cannot unfollow self");
-      return Status::OK;
-    }
-
-    Client *user = nullptr;
-    Client *unfollow_user = nullptr;
-
-    for (auto &client : client_db)
-    {
-      if (client->username == username)
-      {
-        user = client;
-      }
-      if (client->username == unfollow_username)
-      {
-        unfollow_user = client;
-      }
-    }
-
-    if (unfollow_user == nullptr)
-    {
-      reply->set_msg("unfollowed user no exist");
-      return Status::OK;
-    }
-
-    auto it = std::find(user->client_following.begin(), user->client_following.end(), unfollow_user);
-    if (it != user->client_following.end())
-    {
-      user->client_following.erase(it);
-      auto follower_it = std::find(unfollow_user->client_followers.begin(), unfollow_user->client_followers.end(), user);
-      if (follower_it != unfollow_user->client_followers.end())
-      {
-        unfollow_user->client_followers.erase(follower_it);
-      }
-      reply->set_msg("unfollow successful");
-    }
-    else
-    {
-      reply->set_msg("unfollowed user no exist");
-    }
-
-    return Status::OK;
-  }
-
-  // RPC Login
-  Status Login(ServerContext *context, const Request *request, Reply *reply) override
-  {
-
-    /*********
-    YOUR CODE HERE
-    **********/
-    std::cout << "login request from [" << request->username() << "] ";
-    std::string username = request->username();
-    for (Client *c : client_db)
-    {
-      if (c->username == username)
-      {
-        std::cout << "User already exists" << std::endl;
-        reply->set_msg("user already exists");
-        return Status::OK;
-      }
-    }
-
-    // if user does not exist in the client_db, add it into client_db
-    Client *new_client = new Client();
-    new_client->username = username;
-    new_client->connected = true;
-    client_db.push_back(new_client);
-
-    std::cout << "Login successful" << std::endl;
-    reply->set_msg("login successful");
-    return Status::OK;
-  }
-
-  Status Timeline(ServerContext *context,
-                  ServerReaderWriter<Message, Message> *stream) override
-  {
-
-    /*********
-    YOUR CODE HERE
-    **********/
+// ✅ 服务器节点信息结构体
+struct Client {
     std::string username;
-    Message msg;
-
-    // get username from the initial message
-    if (stream->Read(&msg))
-    {
-      username = msg.username();
-    }
-    else
-    {
-      return Status::CANCELLED;
-    }
-
-    std::string timeline_file = "./timelines/" + username + ".txt";
-
-    if (!std::filesystem::exists("./timelines"))
-    {
-      std::filesystem::create_directory("./timelines");
-    }
-
-    // get client of user
-    Client *client = nullptr;
-    for (Client *c : client_db)
-    {
-      if (c->username == username)
-      {
-        client = c;
-        break;
-      }
-    }
-    if (!client)
-    {
-      return Status::CANCELLED;
-    }
-
-    // read the posts of the followings of the user
-    std::vector<Post> history_posts;
-    for (Client *followed_user : client->client_following)
-    {
-      std::string followed_timeline = "./timelines/" + followed_user->username + ".txt";
-      std::ifstream file(followed_timeline);
-      std::string line, post;
-      int count = 0;
-
-      while (std::getline(file, line))
-      {
-        if (line.empty() && !post.empty())
-        {
-          // parse post
-          std::istringstream post_stream(post);
-          std::string line, post_username, post_content;
-          std::time_t post_timestamp;
-
-          while (std::getline(post_stream, line))
-          {
-            if (line[0] == 'T')
-            {
-              post_timestamp = iso8601ToTimeT(line.substr(2));
-            }
-            else if (line[0] == 'U')
-            {
-              post_username = line.substr(2);
-            }
-            else if (line[0] == 'W')
-            {
-              post_content = line.substr(2);
-            }
-          }
-
-          std::time_t follow_timestamp = follow_time[username][post_username];
-          if (post_timestamp >= follow_timestamp)
-          {
-            history_posts.push_back({post_username, post_content, post_timestamp});
-          }
-          post.clear();
-          count++;
-        }
-        else
-        {
-          post += line + "\n";
-        }
-      }
-    }
-
-    std::sort(history_posts.begin(), history_posts.end(), [](const Post &a, const Post &b)
-              { return a.timestamp > b.timestamp; });
-
-    auto n = std::min((size_t)20, history_posts.size());
-    for (int i = 0; i < n; i++)
-    {
-      auto post = history_posts[i];
-      Message message;
-      message.set_username(post.username);
-      message.set_msg(post.post);
-      message.mutable_timestamp()->set_seconds(post.timestamp);
-      message.mutable_timestamp()->set_nanos(0);
-      stream->Write(message);
-    }
-
-    // allow the user to subscribe to new messages from the followings
-    client->stream = stream;
-
-    // listen the inputs from clients
-    while (stream->Read(&msg))
-    {
-      std::string new_post = "T " + google::protobuf::util::TimeUtil::ToString(msg.timestamp()) +
-                             "\nU " + msg.username() +
-                             "\nW " + msg.msg() + "\n\n";
-
-      std::ofstream file(timeline_file, std::ios::app);
-      file << new_post;
-      file.close();
-
-      // send the post to all followers of the user
-      for (Client *follower : client->client_followers)
-      {
-        if (follower->stream)
-        {
-          follower->stream->Write(msg);
-        }
-      }
-    }
-
-    client->stream = nullptr;
-
-    return Status::OK;
-  }
+    std::vector<Client*> client_followers;
+    std::vector<Client*> client_following;
+    grpc::ServerReaderWriter<csce438::Message, csce438::Message>* stream = nullptr;
 };
 
-void RunServer(std::string port_no)
-{
-  std::string server_address = "0.0.0.0:" + port_no;
-  SNSServiceImpl service;
+// ✅ 维护所有客户端
+std::vector<Client*> client_db;
 
-  ServerBuilder builder;
-  builder.AddListeningPort(server_address, grpc::InsecureServerCredentials());
-  builder.RegisterService(&service);
-  std::unique_ptr<Server> server(builder.BuildAndStart());
-  std::cout << "Server listening on " << server_address << std::endl;
-  log(INFO, "Server listening on " + server_address);
+// ✅ SNS 服务器实现
+class SNSServiceImpl final : public SNSService::Service {
+public:
+    Status List(ServerContext* context, const csce438::Request* request, csce438::ListReply* list_reply) override {
+        std::string username = request->username();
+        for (const auto& client : client_db) {
+            list_reply->add_all_users(client->username);
+        }
+        for (const auto& client : client_db) {
+            if (client->username == username) {
+                for (const auto& follower : client->client_followers) {
+                    list_reply->add_followers(follower->username);
+                }
+                break;
+            }
+        }
+        return Status::OK;
+    }
 
-  server->Wait();
+    Status Follow(ServerContext* context, const csce438::Request* request, csce438::Reply* reply) override {
+        std::string follower_name = request->username();
+        std::string followee_name = request->arguments(0);
+
+        if (follower_name == followee_name) {
+            reply->set_msg("cannot follow self");
+            return Status::OK;
+        }
+
+        Client* follower = nullptr;
+        Client* followee = nullptr;
+        for (auto& client : client_db) {
+            if (client->username == follower_name) {
+                follower = client;
+            }
+            if (client->username == followee_name) {
+                followee = client;
+            }
+        }
+
+        if (!followee) {
+            reply->set_msg("followed user no exist");
+            return Status::OK;
+        }
+
+        follower->client_following.push_back(followee);
+        followee->client_followers.push_back(follower);
+        reply->set_msg("followed successful");
+        return Status::OK;
+    }
+
+    Status UnFollow(ServerContext* context, const csce438::Request* request, csce438::Reply* reply) override {
+        std::string username = request->username();
+        std::string unfollow_username = request->arguments(0);
+
+        if (username == unfollow_username) {
+            reply->set_msg("cannot unfollow self");
+            return Status::OK;
+        }
+
+        Client* user = nullptr;
+        Client* unfollow_user = nullptr;
+
+        for (auto& client : client_db) {
+            if (client->username == username) {
+                user = client;
+            }
+            if (client->username == unfollow_username) {
+                unfollow_user = client;
+            }
+        }
+
+        if (!unfollow_user) {
+            reply->set_msg("unfollowed user no exist");
+            return Status::OK;
+        }
+
+        auto it = std::find(user->client_following.begin(), user->client_following.end(), unfollow_user);
+        if (it != user->client_following.end()) {
+            user->client_following.erase(it);
+
+            auto follower_it = std::find(unfollow_user->client_followers.begin(), unfollow_user->client_followers.end(), user);
+            if (follower_it != unfollow_user->client_followers.end()) {
+                unfollow_user->client_followers.erase(follower_it);
+            }
+
+            reply->set_msg("unfollow successful");
+        } else {
+            reply->set_msg("not following user");
+        }
+
+        return Status::OK;
+    }
+
+    Status Login(ServerContext* context, const csce438::Request* request, csce438::Reply* reply) override {
+        std::string username = request->username();
+        for (const auto& client : client_db) {
+            if (client->username == username) {
+                reply->set_msg("user already exists");
+                return Status::OK;
+            }
+        }
+
+        Client* new_client = new Client();
+        new_client->username = username;
+        client_db.push_back(new_client);
+
+        reply->set_msg("login successful");
+        return Status::OK;
+    }
+};
+
+// ✅ 运行 SNS 服务器
+void RunServer(std::string port_no) {
+    std::string server_address = "0.0.0.0:" + port_no;
+    SNSServiceImpl service;
+
+    ServerBuilder builder;
+    builder.AddListeningPort(server_address, grpc::InsecureServerCredentials());
+    builder.RegisterService(&service);
+    std::unique_ptr<Server> server(builder.BuildAndStart());
+    std::cout << "Server listening on " << server_address << std::endl;
+
+    server->Wait();
 }
 
-int main(int argc, char **argv)
-{
+// ✅ 发送心跳信号到 Coordinator
+void HeartbeatThread(std::string server_id, std::string coordinator_ip, std::string coordinator_port, std::string port) {
+    while (true) {
+        grpc::ClientContext context;
+        csce438::ServerInfo request;
+        csce438::Confirmation response;
 
-  std::string port = "3010";
+        request.set_serverid(std::stoi(server_id));
+        request.set_hostname("127.0.0.1");
+        request.set_port(port);
 
-  int opt = 0;
-  while ((opt = getopt(argc, argv, "p:")) != -1)
-  {
-    switch (opt)
-    {
-    case 'p':
-      port = optarg;
-      break;
-    default:
-      std::cerr << "Invalid Command Line Argument\n";
+        std::unique_ptr<csce438::CoordService::Stub> stub =
+            csce438::CoordService::NewStub(grpc::CreateChannel(
+                coordinator_ip + ":" + coordinator_port, grpc::InsecureChannelCredentials()));
+
+        grpc::Status status = stub->Heartbeat(&context, request, &response);
+        if (!status.ok()) {
+            std::cerr << "[TSD] Heartbeat failed: " << status.error_message() << std::endl;
+        }
+
+        sleep(5);
     }
-  }
+}
 
-  std::string log_file_name = std::string("server-") + port;
-  google::InitGoogleLogging(log_file_name.c_str());
-  log(INFO, "Logging Initialized. Server starting...");
-  RunServer(port);
+// ✅ 解析命令行参数并运行服务器
+int main(int argc, char** argv) {
+    std::string cluster_id, server_id, coordinator_ip, coordinator_port, port;
+    
+    int opt = 0;
+    while ((opt = getopt(argc, argv, "c:s:h:k:p:")) != -1) {
+        switch (opt) {
+            case 'c': cluster_id = optarg; break;
+            case 's': server_id = optarg; break;
+            case 'h': coordinator_ip = optarg; break;
+            case 'k': coordinator_port = optarg; break;
+            case 'p': port = optarg; break;
+            default:
+                std::cerr << "Invalid Command Line Argument\n";
+                return -1;
+        }
+    }
 
-  return 0;
+    // ✅ 确保所有参数都被正确解析
+    if (cluster_id.empty() || server_id.empty() || coordinator_ip.empty() || coordinator_port.empty() || port.empty()) {
+        std::cerr << "Usage: ./tsd -c <cluster_id> -s <server_id> -h <coordinator_ip> -k <coordinator_port> -p <port>\n";
+        return -1;
+    }
+
+    std::cout << "Server initialized with:\n"
+              << "Cluster ID: " << cluster_id << "\n"
+              << "Server ID: " << server_id << "\n"
+              << "Coordinator IP: " << coordinator_ip << "\n"
+              << "Coordinator Port: " << coordinator_port << "\n"
+              << "Port: " << port << std::endl;
+
+    // **向 Coordinator 发送心跳**
+    std::thread hb_thread(HeartbeatThread, server_id, coordinator_ip, coordinator_port, port);
+    hb_thread.detach();  // 确保线程不会阻塞主线程
+
+    RunServer(port);
+    return 0;
 }
