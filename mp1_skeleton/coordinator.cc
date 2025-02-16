6710526042b577bd3778662e8d91ae99f77ec0ff
Micahmei <145519069+Micahmei@users.noreply.github.com>
2025-02-15 23:55:54 -0600

    Update coordinator.cc

--- mp1_skeleton/coordinator.cc
+++ mp1_skeleton/coordinator.cc
@@ -64,30 +64,33 @@ std::time_t getTimeNow() {
 class CoordServiceImpl final : public CoordService::Service {
     Status Heartbeat(ServerContext* context, const ServerInfo* serverinfo, Confirmation* confirmation) override {
         std::lock_guard<std::mutex> lock(v_mutex);
-
+    
         int cluster_id = (serverinfo->serverid() - 1) % 3;
         int server_id = serverinfo->serverid();
-
+        bool found = false;
+    
         for (auto& server : clusters[cluster_id]) {
             if (server->serverID == server_id) {
                 server->last_heartbeat = getTimeNow();
                 server->missed_heartbeat = false;
-                LOG(INFO) << "[Coordinator] Received heartbeat from Server " << server_id;
-                confirmation->set_status(true);
-                return Status::OK;
+                found = true;
+                LOG(INFO) << "[Coordinator] Heartbeat updated for Server " << server_id;
+                break;
             }
         }
-
-        // **注册新服务器**
-        zNode* new_server = new zNode{
-            server_id, serverinfo->hostname(), serverinfo->port(), getTimeNow(), false
-        };
-        clusters[cluster_id].push_back(new_server);
-
-        LOG(INFO) << "[Coordinator] Registered new Server " << server_id;
+    
+        if (!found) {
+            zNode* new_server = new zNode{
+                server_id, serverinfo->hostname(), serverinfo->port(), getTimeNow(), false
+            };
+            clusters[cluster_id].push_back(new_server);
+            LOG(INFO) << "[Coordinator] Registered new Server " << server_id;
+        }
+    
         confirmation->set_status(true);
         return Status::OK;
     }
+
 
     Status GetServer(ServerContext* context, const ID* id, ServerInfo* serverinfo) override {
         std::lock_guard<std::mutex> lock(v_mutex);
