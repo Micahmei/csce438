3cc4ea9eee5e17b15bfb247a8259f7388def50ce
Micahmei <145519069+Micahmei@users.noreply.github.com>
2025-04-07 13:19:34 -0500

    Update synchronizer.cc

--- MP2-2/synchronizer.cc
+++ MP2-2/synchronizer.cc
@@ -1,139 +1,162 @@
 #include <iostream>
-#include <fstream>
 #include <thread>
-#include <chrono>
-#include <grpcpp/grpcpp.h>
-#include "coordinator.grpc.pb.h"
+#include <fstream>
+#include <filesystem>
+#include <unordered_set>
 #include <semaphore.h>
 #include <fcntl.h>
-#include <amqpcpp.h>
-#include <amqpcpp/libboostasio.h>
-#include <boost/asio.hpp>
+#include <unistd.h>
+#include <grpc++/grpc++.h>
+#include <google/protobuf/util/time_util.h>
+#include <jsoncpp/json/json.h>
+#include <amqp.h>
+#include <amqp_tcp_socket.h>
+
+#include "coordinator.grpc.pb.h"
+#include "sns.grpc.pb.h"
 
 using grpc::Channel;
 using grpc::ClientContext;
 using grpc::Status;
-using sns::Coordinator;
-using sns::ServerInfoRequest;
-using sns::ServerInfoReply;
+using csce438::CoordService;
+using csce438::ServerInfo;
+using csce438::ServerList;
 
-int cluster_id;
-int sync_id;
-std::string base_dir;
-std::string sem_name = "/timeline_sem";
+int synchID;
+int clusterID;
+std::string coordAddr;
+std::string subdir;
 
-std::string coordinator_ip;
-int coordinator_port;
-
-bool is_master = false;
-
-boost::asio::io_service io_service;
-AMQP::TcpConnection* connection = nullptr;
-AMQP::TcpChannel* channel = nullptr;
-
-// ------------------------------
-// 通用文件写入函数
-// ------------------------------
-void safeWrite(const std::string& filename, const std::string& content) {
-    sem_t* sem = sem_open(sem_name.c_str(), O_CREAT, 0644, 1);
-    sem_wait(sem);
-
-    std::ofstream file(base_dir + "/" + filename, std::ios::app);
-    if (file.is_open()) {
-        file << content << std::endl;
-        file.close();
-    }
-
-    sem_post(sem);
-    sem_close(sem);
+std::string getUserPath(const std::string& user, const std::string& file) {
+    return "./cluster_" + std::to_string(clusterID) + "/" + subdir + "/" + user + "_" + file + ".txt";
 }
 
-// ------------------------------
-// 读取本地用户并广播到 RabbitMQ
-// ------------------------------
-void broadcastUsers() {
-    std::ifstream in(base_dir + "/all_users.txt");
+bool file_contains(const std::string& filename, const std::string& user) {
+    std::ifstream in(filename);
     std::string line;
     while (std::getline(in, line)) {
-        channel->publish("amq.direct", "user_queue", line);
+        if (line == user) return true;
+    }
+    return false;
+}
+
+void append_if_not_exists(const std::string& filename, const std::string& user) {
+    if (!file_contains(filename, user)) {
+        std::ofstream out(filename, std::ios::app);
+        out << user << std::endl;
     }
 }
 
-// ------------------------------
-// 处理收到的用户信息
-// ------------------------------
-void consumeUsers(const std::string& msg) {
-    safeWrite("all_users.txt", msg);
+class RabbitHandler {
+    amqp_connection_state_t conn;
+    amqp_channel_t channel;
+
+public:
+    RabbitHandler() : channel(1) {
+        conn = amqp_new_connection();
+        amqp_socket_t* socket = amqp_tcp_socket_new(conn);
+        amqp_socket_open(socket, "rabbitmq", 5672);
+        amqp_login(conn, "/", 0, 131072, 0, AMQP_SASL_METHOD_PLAIN, "guest", "guest");
+        amqp_channel_open(conn, channel);
+    }
+
+    void declare(const std::string& q) {
+        amqp_queue_declare(conn, channel, amqp_cstring_bytes(q.c_str()), 0, 0, 0, 0, amqp_empty_table);
+    }
+
+    void publish(const std::string& q, const std::string& message) {
+        amqp_basic_publish(conn, channel, amqp_empty_bytes, amqp_cstring_bytes(q.c_str()), 0, 0, NULL, amqp_cstring_bytes(message.c_str()));
+    }
+
+    std::string consume(const std::string& q) {
+        amqp_basic_consume(conn, channel, amqp_cstring_bytes(q.c_str()), amqp_empty_bytes, 0, 1, 0, amqp_empty_table);
+        amqp_envelope_t env;
+        amqp_maybe_release_buffers(conn);
+
+        struct timeval timeout{2, 0};
+        auto res = amqp_consume_message(conn, &env, &timeout, 0);
+        if (res.reply_type != AMQP_RESPONSE_NORMAL) return "";
+
+        std::string msg((char*)env.message.body.bytes, env.message.body.len);
+        amqp_destroy_envelope(&env);
+        return msg;
+    }
+};
+
+void publishAllUsers(RabbitHandler& rh) {
+    std::unordered_set<std::string> users;
+    for (const auto& entry : std::filesystem::directory_iterator("./cluster_" + std::to_string(clusterID) + "/" + subdir)) {
+        auto path = entry.path().string();
+        if (path.find("all_users.txt") != std::string::npos) continue;
+        if (path.find("_followers.txt") != std::string::npos || path.find("_timeline.txt") != std::string::npos) continue;
+        std::string name = entry.path().stem().string();
+        users.insert(name.substr(0, name.find("_")));
+    }
+    Json::Value root;
+    for (const auto& u : users) root["users"].append(u);
+    Json::FastWriter writer;
+    rh.publish("synch" + std::to_string(synchID) + "_users_queue", writer.write(root));
 }
 
-// ------------------------------
-// 启动 RabbitMQ 并订阅队列
-// ------------------------------
-void startRabbitMQ() {
-    AMQP::LibBoostAsioHandler handler(io_service);
-    AMQP::Address addr("amqp://guest:guest@localhost/");
-    connection = new AMQP::TcpConnection(&handler, addr);
-    channel = new AMQP::TcpChannel(connection);
-
-    // 声明并绑定队列
-    channel->declareQueue("user_queue");
-    channel->consume("user_queue").onReceived([](const AMQP::Message& message, uint64_t tag, bool redelivered) {
-        std::string msg(message.body(), message.bodySize());
-        consumeUsers(msg);
-    });
-}
-
-// ------------------------------
-// 与 Coordinator 通信，获取 cluster 信息
-// ------------------------------
-void askCoordinator() {
-    auto stub = Coordinator::NewStub(grpc::CreateChannel(coordinator_ip + ":" + std::to_string(coordinator_port), grpc::InsecureChannelCredentials()));
-
-    ServerInfoRequest req;
-    ServerInfoReply rep;
-    ClientContext ctx;
-
-    req.set_clientid(sync_id); // reuse sync_id
-    stub->GetClusterForClient(&ctx, req, &rep);
-
-    cluster_id = rep.clusterid();
-    base_dir = "./cluster" + std::to_string(cluster_id) + "/" + std::to_string(sync_id <= 3 ? 1 : 2);
-    is_master = (sync_id <= 3);
-}
-
-// ------------------------------
-// 定时广播线程
-// ------------------------------
-void syncThread() {
-    while (true) {
-        std::this_thread::sleep_for(std::chrono::seconds(30));
-        if (is_master) {
-            broadcastUsers();
+void consumeAllUsers(RabbitHandler& rh) {
+    for (int i = 1; i <= 6; ++i) {
+        auto msg = rh.consume("synch" + std::to_string(i) + "_users_queue");
+        if (msg.empty()) continue;
+        Json::Value root;
+        Json::Reader reader;
+        if (reader.parse(msg, root)) {
+            for (auto& u : root["users"]) {
+                std::string filename = "./cluster_" + std::to_string(clusterID) + "/" + subdir + "/all_users.txt";
+                append_if_not_exists(filename, u.asString());
+            }
         }
     }
 }
 
-// ------------------------------
-// 主函数入口
-// ------------------------------
+void synchronizerLoop(RabbitHandler& rh) {
+    while (true) {
+        publishAllUsers(rh);
+        consumeAllUsers(rh);
+        std::this_thread::sleep_for(std::chrono::seconds(5));
+    }
+}
+
 int main(int argc, char** argv) {
-    if (argc != 9) {
-        std::cerr << "Usage: ./synchronizer -h <coordIP> -k <coordPort> -p <port> -i <syncID>" << std::endl;
-        return 1;
+    std::string coord_ip = "127.0.0.1", coord_port = "9090", port = "9091";
+    synchID = 1;
+
+    int opt;
+    while ((opt = getopt(argc, argv, "h:k:p:i:")) != -1) {
+        switch (opt) {
+            case 'h': coord_ip = optarg; break;
+            case 'k': coord_port = optarg; break;
+            case 'p': port = optarg; break;
+            case 'i': synchID = std::stoi(optarg); break;
+        }
     }
 
-    coordinator_ip = argv[2];
-    coordinator_port = std::stoi(argv[4]);
-    sync_id = std::stoi(argv[8]);
+    clusterID = ((synchID - 1) % 3) + 1;
+    subdir = (synchID <= 3) ? "1" : "2";
+    coordAddr = coord_ip + ":" + coord_port;
 
-    askCoordinator();
-    std::cout << "[Synchronizer] Cluster " << cluster_id << ", ID " << sync_id << " (" << (is_master ? "Master" : "Slave") << ")" << std::endl;
+    ServerInfo info;
+    info.set_serverid(synchID);
+    info.set_clusterid(clusterID);
+    info.set_hostname("127.0.0.1");
+    info.set_port(port);
+    info.set_type("synchronizer");
+    info.set_ismaster(synchID <= 3);
 
-    startRabbitMQ();
+    auto channel = grpc::CreateChannel(coordAddr, grpc::InsecureChannelCredentials());
+    std::unique_ptr<CoordService::Stub> coord_stub = CoordService::NewStub(channel);
+    Confirmation conf;
+    ClientContext ctx;
+    coord_stub->Heartbeat(&ctx, info, &conf);
 
-    std::thread t(syncThread);
-    io_service.run();
+    RabbitHandler rh;
+    rh.declare("synch" + std::to_string(synchID) + "_users_queue");
 
+    std::thread t(synchronizerLoop, std::ref(rh));
     t.join();
     return 0;
 }
