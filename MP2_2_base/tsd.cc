70dbdafd3d8c6dd76629fb4c9895d2bf6f974c93
Micahmei <145519069+Micahmei@users.noreply.github.com>
2025-04-09 00:45:03 -0500

    Update tsd.cc

--- MP2_2_base/tsd.cc
+++ MP2_2_base/tsd.cc
@@ -35,7 +35,7 @@
 
 #include <google/protobuf/timestamp.pb.h>
 #include <google/protobuf/duration.pb.h>
-
+#include <thread>
 #include <fstream>
 #include <filesystem>
 #include <iostream>
@@ -55,9 +55,11 @@
 
 #include "sns.grpc.pb.h"
 #include "coordinator.grpc.pb.h"
-#include <thread>
+
+#include "sem_file.h"
+
 using csce438::Confirmation;
-using csce438::Coordinator;
+using csce438::CoordService;
 using csce438::ListReply;
 using csce438::Message;
 using csce438::Reply;
@@ -75,30 +77,6 @@ using grpc::ServerReader;
 using grpc::ServerReaderWriter;
 using grpc::ServerWriter;
 using grpc::Status;
-
-// return local time
-std::time_t iso8601ToTimeT(const std::string &iso8601)
-{
-  std::tm tm = {};
-  std::istringstream ss(iso8601);
-
-  ss >> std::get_time(&tm, "%Y-%m-%dT%H:%M:%SZ");
-
-  if (ss.fail())
-  {
-    std::cerr << "Error: Failed to parse timestamp: " << iso8601 << std::endl;
-    return 0;
-  }
-
-  return timegm(&tm);
-}
-
-struct Post
-{
-  std::string username;
-  std::string post;
-  std::time_t timestamp;
-};
 
 struct Client
 {
@@ -120,10 +98,14 @@ std::vector<Client *> client_db;
 class SNSServiceImpl final : public SNSService::Service
 {
   std::unordered_map<std::string, std::unordered_map<std::string, std::time_t>> follow_time;
-  std::unique_ptr<Coordinator::Stub> coordinator_stub;
+  std::unique_ptr<CoordService::Stub> coordinator_stub;
 
 public:
-  SNSServiceImpl(std::shared_ptr<Channel> channel) : coordinator_stub(Coordinator::NewStub(channel)) {}
+  uint64_t cluster_id;
+  uint64_t server_id;
+  std::string data_dir;
+
+  SNSServiceImpl(std::shared_ptr<Channel> channel) : coordinator_stub(CoordService::NewStub(channel)) {}
 
   bool SendHeartbeat(const uint64_t &cluster_id, const uint64_t &server_id, const std::string &ip, const std::string &port)
   {
@@ -156,23 +138,17 @@ private:
     YOUR CODE HERE
     **********/
     std::string username = request->username();
-    for (const auto &client : client_db)
+
+    auto all_users = read_users_from_file(data_dir + "/all_users.txt");
+    for (const auto &user : all_users)
     {
-      list_reply->add_all_users(client->username);
+      list_reply->add_all_users(user);
     }
 
-    Client *current_user = nullptr;
-    for (const auto &client : client_db)
+    auto followers = read_users_from_file(data_dir + "/" + username + "_follower_list.txt");
+    for (const auto &follower : followers)
     {
-      if (client->username == username)
-      {
-        current_user = client;
-        break;
-      }
-    }
-    for (const auto &follower : current_user->client_followers)
-    {
-      list_reply->add_followers(follower->username);
+      list_reply->add_followers(follower);
     }
 
     return Status::OK;
@@ -194,43 +170,25 @@ private:
       return Status::OK;
     }
 
-    Client *follower = nullptr;
-    Client *followee = nullptr;
-
-    for (auto &client : client_db)
-    {
-      if (client->username == follower_name)
-      {
-        follower = client;
-      }
-      if (client->username == followee_name)
-      {
-        followee = client;
-      }
-    }
-
-    if (followee == nullptr)
+    bool followee_exist = find_user_from_file(data_dir + "/all_users.txt", followee_name);
+    if (!followee_exist)
     {
       reply->set_msg("followed user no exist");
       return Status::OK;
     }
 
-    for (const auto &following : follower->client_following)
+    bool already_following = find_user_from_file(data_dir + "/" + follower_name + "_following_list.txt", followee_name);
+    if (already_following)
     {
-      if (following->username == followee_name)
-      {
-        reply->set_msg("already following user");
-        return Status::OK;
-      }
+      reply->set_msg("already following user");
+      return Status::OK;
     }
 
-    follower->client_following.push_back(followee);
-    followee->client_followers.push_back(follower);
-
-    follow_time[follower_name][followee_name] = std::time(nullptr);
+    write_new_user(data_dir + "/" + follower_name + "_following_list.txt", followee_name);
+    write_new_user(data_dir + "/" + followee_name + "_follower_list.txt", follower_name);
+    write_follow_time(data_dir + "/follow_time.txt", follower_name, followee_name, std::time(nullptr));
 
     reply->set_msg("followed successful");
-
     return Status::OK;
   }
 
@@ -297,21 +255,24 @@ private:
     **********/
     std::cout << "login request from [" << request->username() << "] ";
     std::string username = request->username();
+    bool user_exist = false;
     for (Client *c : client_db)
     {
       if (c->username == username)
-      {
-        std::cout << "User already exists" << std::endl;
-        reply->set_msg("user already exists");
-        return Status::OK;
-      }
+        user_exist = true;
     }
+    if (user_exist == false)
+    {
+      std::string users_file = data_dir + "/all_users.txt";
+      write_new_user(users_file, username);
 
-    // if user does not exist in the client_db, add it into client_db
-    Client *new_client = new Client();
-    new_client->username = username;
-    new_client->connected = true;
-    client_db.push_back(new_client);
+      // if user does not exist in the client_db, add it into client_db
+      std::cout << "new user login" << std::endl;
+      Client *new_client = new Client();
+      new_client->username = username;
+      new_client->connected = true;
+      client_db.push_back(new_client);
+    }
 
     std::cout << "Login successful" << std::endl;
     reply->set_msg("login successful");
@@ -338,13 +299,6 @@ private:
       return Status::CANCELLED;
     }
 
-    std::string timeline_file = "./timelines/" + username + ".txt";
-
-    if (!std::filesystem::exists("./timelines"))
-    {
-      std::filesystem::create_directory("./timelines");
-    }
-
     // get client of user
     Client *client = nullptr;
     for (Client *c : client_db)
@@ -360,72 +314,97 @@ private:
       return Status::CANCELLED;
     }
 
+    auto following_users = read_users_from_file(data_dir + "/" + username + "_following_list.txt");
+    auto follow_time_map = read_follow_time(data_dir + "/follow_time.txt");
+
     // read the posts of the followings of the user
+    std::unordered_map<std::string, std::time_t> last_update_times;
     std::vector<Post> history_posts;
-    for (Client *followed_user : client->client_following)
+    for (auto following_user : following_users)
     {
-      std::string followed_timeline = "./timelines/" + followed_user->username + ".txt";
-      std::ifstream file(followed_timeline);
-      std::string line, post;
-      int count = 0;
-
-      while (std::getline(file, line))
-      {
-        if (line.empty() && !post.empty())
-        {
-          // parse post
-          std::istringstream post_stream(post);
-          std::string line, post_username, post_content;
-          std::time_t post_timestamp;
-
-          while (std::getline(post_stream, line))
-          {
-            if (line[0] == 'T')
-            {
-              post_timestamp = iso8601ToTimeT(line.substr(2));
-            }
-            else if (line[0] == 'U')
-            {
-              post_username = line.substr(2);
-            }
-            else if (line[0] == 'W')
-            {
-              post_content = line.substr(2);
-            }
-          }
-
-          std::time_t follow_timestamp = follow_time[username][post_username];
-          if (post_timestamp >= follow_timestamp)
-          {
-            history_posts.push_back({post_username, post_content, post_timestamp});
-          }
-          post.clear();
-          count++;
-        }
-        else
-        {
-          post += line + "\n";
-        }
-      }
+      auto posts = read_timeline_from_file(data_dir + "/" + following_user + "_timeline.txt");
+      history_posts.insert(history_posts.end(), posts.begin(), posts.end());
     }
-
-    std::sort(history_posts.begin(), history_posts.end(), [](const Post &a, const Post &b)
+    std::sort(history_posts.begin(), history_posts.end(),
+              [](const Post &a, const Post &b)
               { return a.timestamp > b.timestamp; });
-
     auto n = std::min((size_t)20, history_posts.size());
-    for (int i = 0; i < n; i++)
+    for (int i = n - 1; i >= 0; i--)
     {
       auto post = history_posts[i];
-      Message message;
-      message.set_username(post.username);
-      message.set_msg(post.post);
-      message.mutable_timestamp()->set_seconds(post.timestamp);
-      message.mutable_timestamp()->set_nanos(0);
-      stream->Write(message);
+      if (post.timestamp >= follow_time_map[username][post.username])
+      {
+        last_update_times[post.username] = post.timestamp;
+        Message message;
+        message.set_username(post.username);
+        message.set_msg(post.post);
+        message.mutable_timestamp()->set_seconds(post.timestamp);
+        message.mutable_timestamp()->set_nanos(0);
+        stream->Write(message);
+      }
     }
 
     // allow the user to subscribe to new messages from the followings
     client->stream = stream;
+
+    // use a thread to read the new posts from the following users periodly
+    std::string temp_data_dir = data_dir;
+    bool stop_thread = false;
+    std::thread read_followings_posts_thread(
+        [&temp_data_dir, &username, &stream, &stop_thread, &last_update_times, &follow_time_map]()
+        {
+          while (!stop_thread)
+          {
+            auto following_users = read_users_from_file(temp_data_dir + "/" + username + "_following_list.txt");
+            std::vector<Post> new_posts;
+            for (const auto &following : following_users)
+            {
+              auto posts = read_timeline_from_file(temp_data_dir + "/" + following + "_timeline.txt");
+              new_posts.insert(new_posts.end(), posts.begin(), posts.end());
+            }
+            std::sort(new_posts.begin(), new_posts.end(),
+                      [](const Post &a, const Post &b)
+                      { return a.timestamp < b.timestamp; });
+            for (auto &post : new_posts)
+            {
+              std::time_t last_update_time = follow_time_map[username][post.username];
+              auto it = last_update_times.find(post.username);
+              if (it != last_update_times.end())
+              {
+                last_update_time = it->second;
+              }
+
+              // std::cout << post.timestamp << " " << last_update_time << std::endl;
+              if (post.timestamp > last_update_time)
+              {
+                Message message;
+                message.set_username(post.username);
+                message.set_msg(post.post);
+                message.mutable_timestamp()->set_seconds(post.timestamp);
+                message.mutable_timestamp()->set_nanos(0);
+                try
+                {
+                  if (stream)
+                  {
+                    bool success = stream->Write(message);
+                    if (!success)
+                    {
+                      stop_thread = true;
+                      break;
+                    }
+                  }
+                }
+                catch (const std::exception &e)
+                {
+                  stop_thread = true;
+                  break;
+                }
+                last_update_times[post.username] = post.timestamp;
+              }
+            }
+            std::this_thread::sleep_for(std::chrono::seconds(10));
+          }
+        });
 
     // listen the inputs from clients
     while (stream->Read(&msg))
@@ -434,24 +413,17 @@ private:
                              "\nU " + msg.username() +
                              "\nW " + msg.msg() + "\n\n";
 
-      std::ofstream file(timeline_file, std::ios::app);
-      file << new_post;
-      file.close();
-
-      // send the post to all followers of the user
-      for (Client *follower : client->client_followers)
-      {
-        if (follower->stream)
-        {
-          follower->stream->Write(msg);
-        }
-      }
+      std::string timeline_file = data_dir + "/" + username + "_timeline.txt";
+      write_timeline_to_file(timeline_file, new_post);
     }
 
+    stop_thread = true;
+    read_followings_posts_thread.join();
     client->stream = nullptr;
 
     return Status::OK;
   }
+
   Status CheckConnection(ServerContext *context, const Request *request, Reply *response) override
   {
     return Status::OK;
@@ -463,6 +435,17 @@ void RunServer(uint64_t server_id, std::string ip, std::string port, uint64_t cl
   grpc::ChannelArguments ch_args;
   ch_args.SetInt(GRPC_ARG_ENABLE_HTTP_PROXY, 0);
   SNSServiceImpl service(grpc::CreateCustomChannel(cord_ip + ":" + cord_port, grpc::InsecureChannelCredentials(), ch_args));
+
+  service.cluster_id = cluster_id;
+  service.server_id = server_id;
+
+  service.data_dir = "./cluster_" + std::to_string(cluster_id) + "/" + std::to_string(server_id);
+  if (!std::filesystem::exists("./cluster_" + std::to_string(cluster_id)))
+  {
+    std::filesystem::create_directory("./cluster_" + std::to_string(cluster_id));
+    if (!std::filesystem::exists(service.data_dir))
+      std::filesystem::create_directory(service.data_dir);
+  }
 
   std::thread heartbeat_thread(
       [&service, &cluster_id, &server_id, &ip, &port]()
